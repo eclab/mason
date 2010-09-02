1dc3b91c443cbc6ce6f1e30d1080f79932f855e9
mcoletti <mcoletti@e22d1478-1c87-2d72-604f-1c9cf2634165>
2010-09-02 20:09:40 +0000

    Added computeBounds() and isWithinBounds(), which are used for properly placing a GeomGridField within a Display2D.
    git-svn-id: https://mason.googlecode.com/svn/trunk@338 e22d1478-1c87-2d72-604f-1c9cf2634165

--- contrib/geomason/sim/util/geo/GeometryUtilities.java
+++ contrib/geomason/sim/util/geo/GeometryUtilities.java
@@ -2,10 +2,12 @@ package sim.util.geo;
 
 import java.awt.geom.AffineTransform;
 import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D.Double;
 import java.util.Comparator;
 
 import sim.portrayal.DrawInfo2D;
 import com.vividsolutions.jts.geom.Envelope;
+import sim.field.geo.GeomGridField;
 
 public class GeometryUtilities {
 
@@ -54,5 +56,68 @@ public class GeometryUtilities {
      			return af1.name.compareTo(af2.name) ;
      		}
      };
+
+
+         /** compute the MBR for the grid field in display coordinates
+     *
+     * This is used to determine the display bounds for the grid field for
+     * Display2D.attach().
+     *
+     * @param outer denotes MBR that maps to display window in world coordinates
+     * @param drawInfo describes the view port into which we'll be displaying the grid field
+     * @param gridField for which we wish to find bounds in display coordinates
+     *
+     * @return grid field bounds in display coordinates; will return drawInfo bounds if grid does not intersect given outer MBR
+     */
+    static public java.awt.geom.Rectangle2D.Double computeBounds(final Envelope outer, final DrawInfo2D drawInfo, final GeomGridField gridField)
+    {
+        java.awt.geom.Rectangle2D.Double bounds = (Double) drawInfo.draw.clone();
+
+        AffineTransform transform = GeometryUtilities.worldToScreenTransform(outer, drawInfo);
+
+        if (outer.contains(gridField.MBR) ||
+            gridField.MBR.contains(outer) ||
+            outer.intersects(gridField.MBR))
+        {
+            // Pretty straightforward; just translate all the corners into display coordinates
+
+            Point2D.Double srcMinPoint = new Point2D.Double(gridField.MBR.getMinX(), gridField.MBR.getMaxY());
+            Point2D destMinPoint = transform.transform(srcMinPoint, null);
+
+            Point2D.Double srcMaxPoint = new Point2D.Double(gridField.MBR.getMaxX(), gridField.MBR.getMinY());
+            Point2D destMaxPoint = transform.transform(srcMaxPoint, null);
+
+            bounds.setRect(destMinPoint.getX(), destMinPoint.getY(), destMaxPoint.getX() - destMinPoint.getX(), destMaxPoint.getY() - destMinPoint.getY());
+
+        } else // badness happened
+        {
+            // not good if the grid isn't even within the outer MBR; this likely means that
+            // 'outer' and 'gridField' are using different spatial reference systems
+            System.err.println("Warning: raster not in display");
+        }
+
+        return bounds;
+    }
+
+
+    /**
+     * @param outer denotes MBR that maps to display window in world coordinates
+     * @param gridField for which we wish to find bounds in display coordinates
+     *
+     * @returns true iff 'gridField' is within, intersects, or covers 'outer', else returns false
+     *
+     * Can be used as check for computeBounds()
+     */
+    static public boolean isWithinBounds(final Envelope outer, final GeomGridField gridField)
+    {
+        if (outer.contains(gridField.MBR) ||
+            gridField.MBR.contains(outer) ||
+            outer.intersects(gridField.MBR))
+        {
+            return true;
+        }
+        return false;
+    }
+
      
 }
