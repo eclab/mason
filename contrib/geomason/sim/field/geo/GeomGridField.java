5f4ba3edae65815d6d93a58328d52b46a92d0af7
mcoletti <mcoletti@e22d1478-1c87-2d72-604f-1c9cf2634165>
2010-09-24 18:20:45 +0000

    Added isInGrid() and toPolygon().
    git-svn-id: https://mason.googlecode.com/svn/trunk@348 e22d1478-1c87-2d72-604f-1c9cf2634165

--- contrib/geomason/sim/field/geo/GeomGridField.java
+++ contrib/geomason/sim/field/geo/GeomGridField.java
@@ -3,7 +3,9 @@ package sim.field.geo;
 import com.vividsolutions.jts.geom.Coordinate;
 import com.vividsolutions.jts.geom.Envelope;
 import com.vividsolutions.jts.geom.GeometryFactory;
+import com.vividsolutions.jts.geom.LinearRing;
 import com.vividsolutions.jts.geom.Point;
+import com.vividsolutions.jts.geom.Polygon;
 import sim.field.grid.Grid2D;
 
 /** A georeferenced area represented by a grid
@@ -134,6 +136,20 @@ public class GeomGridField extends GeomField
     }
 
 
+
+    /**
+     * @return true if (x,y) within grid boundaries
+     */
+    public boolean isInGrid(int x, int y)
+    {
+        if ( x < 0 || y < 0 || x >= getGridWidth() || y >= getGridHeight() )
+        {
+            return false;
+        }
+        return true;
+    }
+
+
     /** Return a Point corresponding to center of grid cell
      *
      * @param x in pixel coordinates
@@ -144,7 +160,7 @@ public class GeomGridField extends GeomField
     {
         assert x >= 0 && y >= 0 && x < getGridWidth() && y < getGridHeight() : "x: " + x + "y: " + y;
 
-        if ( x < 0 || y < 0 || x >= getGridWidth() || y >= getGridHeight() )
+        if ( ! isInGrid(x,y) )
         {
             throw new IndexOutOfBoundsException();
         }
@@ -163,6 +179,66 @@ public class GeomGridField extends GeomField
 
         return point;
     }
+
+
+    /** Return a Polygon corresponding to the grid cell perimeter
+     *
+     * @param x in pixel coordinates
+     * @param y in pixel coordinates
+     * @return Polygon representing grid cell area
+     */
+    public Polygon toPolygon(int x, int y)
+    {
+        assert x >= 0 && y >= 0 && x < getGridWidth() && y < getGridHeight() : "x: " + x + "y: " + y;
+
+        if ( ! isInGrid(x,y) )
+        {
+            throw new IndexOutOfBoundsException();
+        }
+
+        Coordinate[] coordinates = new Coordinate[5];
+        double x_orig = getMBR().getMinX();
+        double y_orig = getMBR().getMinY(); // XXX getMaxY() instead?
+
+
+        // Bottom left corner
+        coordinates[0] = new Coordinate();
+        coordinates[0].x = x_orig + (x * getPixelWidth());
+        coordinates[0].y = y_orig + (y * getPixelHeight());
+        coordinates[0].z = 0.0;
+
+        // Upper left corner
+        coordinates[1] = new Coordinate();
+        coordinates[1].x = x_orig + (x * getPixelWidth());
+        coordinates[1].y = y_orig + ((y + 1) * getPixelHeight());
+        coordinates[1].z = 0.0;
+
+        // Upper right corner
+        coordinates[2] = new Coordinate();
+        coordinates[2].x = x_orig + ((x + 1) * getPixelWidth());
+        coordinates[2].y = y_orig + ((y + 1) * getPixelHeight());
+        coordinates[2].z = 0.0;
+
+        // Finally, lower right corner
+        coordinates[3] = new Coordinate();
+        coordinates[3].x = x_orig + ((x + 1) * getPixelWidth());
+        coordinates[3].y = y_orig + (y * getPixelHeight());
+        coordinates[3].z = 0.0;
+
+        // Close the ring
+        coordinates[4] = new Coordinate();
+        coordinates[4].x = x_orig + (x * getPixelWidth());
+        coordinates[4].y = y_orig + (y * getPixelHeight());
+        coordinates[4].z = 0.0;
+
+        
+        LinearRing ring = geometryFactory.createLinearRing(coordinates);
+
+        Polygon polygon = geometryFactory.createPolygon(ring, null); // null means no inner rings; i.e., no hollow parts
+
+        return polygon;
+    }
+
     
 
     /** Used to create geometry as needed
