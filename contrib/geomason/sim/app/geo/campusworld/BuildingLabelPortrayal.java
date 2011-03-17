3da211c5aa05cbb842ce1b8c9fa1ba66da3d9da8
kemsulli@gmail.com <kemsulli@gmail.com@e22d1478-1c87-2d72-604f-1c9cf2634165>
2011-03-17 18:40:57 +0000

    git-svn-id: https://mason.googlecode.com/svn/trunk@420 e22d1478-1c87-2d72-604f-1c9cf2634165

---
+++ contrib/geomason/sim/app/geo/campusworld/BuildingLabelPortrayal.java
@@ -0,0 +1,43 @@
+package sim.app.geo.campusworld;
+
+import java.awt.Paint;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import com.vividsolutions.jts.geom.Geometry;
+
+import sim.portrayal.DrawInfo2D;
+import sim.portrayal.SimplePortrayal2D;
+import sim.portrayal.simple.LabelledPortrayal2D;
+import sim.util.geo.AttributeField;
+import sim.util.geo.GeometryUtilities;
+import sim.util.geo.MasonGeometry;
+
+public class BuildingLabelPortrayal extends LabelledPortrayal2D
+{
+	private static final long serialVersionUID = 1L;
+	
+	public BuildingLabelPortrayal(SimplePortrayal2D child, Paint paint)
+	{
+		super(child, null, paint, true);
+	}
+
+	
+	public String getLabel(Object object, DrawInfo2D info)
+	{
+		String label = "";
+		if (object instanceof MasonGeometry)
+		{
+			MasonGeometry mg = (MasonGeometry) object;
+			AttributeField key = new AttributeField("NAME");
+			Geometry g = mg.getGeometry();
+			ArrayList<AttributeField> attrs = (ArrayList<AttributeField>) g.getUserData();
+			int index = Collections.binarySearch(attrs, key, GeometryUtilities.attrFieldCompartor);
+
+			if (index >= 0)
+				label = (String)attrs.get(index).value;
+		}
+		return label;
+
+	}
+}
