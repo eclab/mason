4c893f087d1f90a32e13961e4287a43f273a7bd5
mcoletti <mcoletti@e22d1478-1c87-2d72-604f-1c9cf2634165>
2011-06-22 17:32:40 +0000

    Added license text.
    git-svn-id: https://mason.googlecode.com/svn/trunk@478 e22d1478-1c87-2d72-604f-1c9cf2634165

--- contrib/geomason/sim/app/geo/networkworld/Agent.java
+++ contrib/geomason/sim/app/geo/networkworld/Agent.java
@@ -1,3 +1,10 @@
+/* 
+Copyright 2011 by Mark Coletti, Keith Sullivan, Sean Luke, and
+George Mason University Mason University Licensed under the Academic
+Free License version 3.0
+
+See the file "LICENSE" for more information
+*/
 /**
  * Agent.java
  *
