55d34e771c4b1b288cabc76de1058ddcb65e3995
sean@cs.gmu.edu <sean@cs.gmu.edu@e22d1478-1c87-2d72-604f-1c9cf2634165>
2012-02-03 12:53:54 +0000

    yo
    git-svn-id: https://mason.googlecode.com/svn/trunk@604 e22d1478-1c87-2d72-604f-1c9cf2634165

--- mason/sim/util/Properties.java
+++ mason/sim/util/Properties.java
@@ -202,6 +202,7 @@ public abstract class Properties implements java.io.Serializable
             }
         }
     
+    NumberFormat format = NumberFormat.getInstance();
     /**
      * Replaces <code>Double.valueOf()</code> for the GUI.
      * 
@@ -219,7 +220,6 @@ public abstract class Properties implements java.io.Serializable
      * @return a <code>Double</code> instance representing <code>s</code>.
      * @throws ParseException if the specified <code>s</code> string cannot be parsed as a <code>Double</code>
      */
-    NumberFormat format = NumberFormat.getInstance();
     Double betterDoubleValueOf(String s) throws ParseException
     	{
         return Double.valueOf( format.parse(s).doubleValue() );
