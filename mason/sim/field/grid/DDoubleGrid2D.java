71d0081fedf287820ec45485a16e3f725b2ed28c
Haoliang Wang <haoliangx@users.noreply.github.com>
2018-01-09 22:00:36 -0500

    9-cell stencil

--- mason/sim/field/grid/DDoubleGrid2D.java
+++ mason/sim/field/grid/DDoubleGrid2D.java
@@ -8,7 +8,6 @@ import static mpi.MPI.slice;
 public class DDoubleGrid2D extends DoubleGrid2D {
 
 	public double[] field;
-	//DoubleBuffer buf;
 	public int width, height, np, pw, ph, psize, aoi;
 	public int dims[], coords[];
 	CartParms topoParams;
@@ -22,24 +21,22 @@ public class DDoubleGrid2D extends DoubleGrid2D {
 		this.height = height;
 		this.aoi = aoi;
 
-		// Init MPI Topology
-		dims = new int[2];
+		// Init MPI Cartesian Topology (4 neighbors)
+		dims = new int[] {0, 0};
 		coords = new int[2];
 		boolean periods[] = {true, true};
-		dims[0] = 0;
-		dims[1] = 0;
 
 		try {
 			np = MPI.COMM_WORLD.getSize();
 			CartComm.createDims(np, dims);
 			comm = ((Intracomm)MPI.COMM_WORLD).createCart(dims, periods, false);
 			topoParams = comm.getTopo();
-			coords[0] = topoParams.getCoord(0);
-			coords[1] = topoParams.getCoord(1);
 		} catch (MPIException e) {
 			e.printStackTrace();
 			System.exit(-1);
 		}
+		coords[0] = topoParams.getCoord(0);
+		coords[1] = topoParams.getCoord(1);
 
 		// Init local storage
 		pw = width / dims[0];
@@ -48,8 +45,6 @@ public class DDoubleGrid2D extends DoubleGrid2D {
 		field = new double[psize];
 		for (int i = 0; i < psize; i++)
 			field[i] = initialValue;
-
-		//buf = MPI.newDoubleBuffer(psize);
 
 		// Setup MPI types for Halo Exchange
 		try {
@@ -101,7 +96,7 @@ public class DDoubleGrid2D extends DoubleGrid2D {
 		field[lx * (ph + 2 * aoi) + ly] = val;
 	}
 
-	public void sync() throws MPIException{
+	public void sync() throws MPIException {
 		// Prepare data
 		int sendsize = 2 * (pw + ph) * aoi;
 		byte[] sendbuf = new byte[sendsize * 8];
@@ -109,36 +104,37 @@ public class DDoubleGrid2D extends DoubleGrid2D {
 		int[] pos = new int[4], count = new int[4];
 		int lastPos = 0;
 
-		// pack north - field[aoi][aoi]
-		pos[0] = lastPos;
-		lastPos = comm.pack(slice(field, aoi * (ph + 2 * aoi) + aoi), 1, wtype, sendbuf, lastPos);
-		count[0] = lastPos - pos[0];
-
-		// pack south - field[pw][aoi]
-		pos[1] = lastPos;
-		lastPos = comm.pack(slice(field, pw * (ph + 2 * aoi) + aoi), 1, wtype, sendbuf, lastPos);
-		count[1] = lastPos - pos[1];
-
-		// pack west - field[aoi][aoi]
-		pos[2] = lastPos;
-		lastPos = comm.pack(slice(field, aoi * (ph + 2 * aoi) + aoi), 1, htype, sendbuf, lastPos);
-		count[2] = lastPos - pos[2];
-
-		// pack east - field[aoi][ph]
-		pos[3] = lastPos;
-		lastPos = comm.pack(slice(field, aoi * (ph + 2 * aoi) + ph), 1, htype, sendbuf, lastPos);
-		count[3] = lastPos - pos[3];
+		// Pack data into byte array
+		pos[0] = lastPos; lastPos = comm.pack(slice(field, idx(aoi, aoi)), 1, wtype, sendbuf, lastPos); count[0] = lastPos - pos[0]; // pack north - field[aoi][aoi]
+		pos[1] = lastPos; lastPos = comm.pack(slice(field, idx(pw , aoi)), 1, wtype, sendbuf, lastPos); count[1] = lastPos - pos[1]; // pack south - field[pw][aoi]
+		pos[2] = lastPos; lastPos = comm.pack(slice(field, idx(aoi, aoi)), 1, htype, sendbuf, lastPos); count[2] = lastPos - pos[2]; // pack west - field[aoi][aoi]
+		pos[3] = lastPos; lastPos = comm.pack(slice(field, idx(aoi, ph)), 1, htype, sendbuf, lastPos); count[3] = lastPos - pos[3]; // pack east - field[aoi][ph]
 
 		// Exchange
 		comm.neighborAllToAllv(sendbuf, count, pos, MPI.BYTE, recvbuf, count, pos, MPI.BYTE);
 
-		// unpack north
-		comm.unpack(recvbuf, pos[0], slice(field, aoi), 1, wtype);
-		// unpack south
-		comm.unpack(recvbuf, pos[1], slice(field, (pw + aoi) * (ph + 2 * aoi) + aoi), 1, wtype);
-		// unpack west
-		comm.unpack(recvbuf, pos[2], slice(field, aoi * (ph + 2 * aoi)), 1, htype);
-		// unpack east
-		comm.unpack(recvbuf, pos[3], slice(field, aoi * (ph + 2 * aoi) + ph + aoi), 1, htype);
+		// Unpack
+		comm.unpack(recvbuf, pos[0], slice(field, idx(0, aoi)), 1, wtype); // unpack north
+		comm.unpack(recvbuf, pos[1], slice(field, idx(pw + aoi, aoi)), 1, wtype); // unpack south
+		comm.unpack(recvbuf, pos[2], slice(field, idx(aoi, 0)) , 1, htype); // unpack west
+		comm.unpack(recvbuf, pos[3], slice(field, idx(aoi, ph + aoi)), 1, htype); // unpack east
+
+		// four corners (9-cell stencil)
+		lastPos = 0;
+		pos[0] = lastPos; lastPos = comm.pack(slice(field, idx(aoi, aoi + ph)), 1, ctype, sendbuf, lastPos); count[0] = lastPos - pos[0];
+		pos[1] = lastPos; lastPos = comm.pack(slice(field, idx(pw, 0)), 1, ctype, sendbuf, lastPos); count[1] = lastPos - pos[1];
+		pos[2] = lastPos; lastPos = comm.pack(slice(field, idx(0, aoi)), 1, ctype, sendbuf, lastPos); count[2] = lastPos - pos[2];
+		pos[3] = lastPos; lastPos = comm.pack(slice(field, idx(pw + aoi, ph)), 1, ctype, sendbuf, lastPos); count[3] = lastPos - pos[3];
+
+		comm.neighborAllToAllv(sendbuf, count, pos, MPI.BYTE, recvbuf, count, pos, MPI.BYTE);
+
+		comm.unpack(recvbuf, pos[0], slice(field, idx(0, 0)), 1, ctype);
+		comm.unpack(recvbuf, pos[1], slice(field, idx(pw + aoi, ph + aoi)), 1, ctype);
+		comm.unpack(recvbuf, pos[2], slice(field, idx(pw + aoi, 0)) , 1, ctype);
+		comm.unpack(recvbuf, pos[3], slice(field, idx(0, ph + aoi)), 1, ctype);
+	}
+
+	private int idx(int x, int y) {
+		return x * (ph + 2 * aoi) + y;
 	}
 }
