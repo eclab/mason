afc5214aa106ba12c214e93b66f411d46a8ad607
Lilas Dinh <lvkdinh@gmail.com>
2019-01-25 12:50:39 -0500

    Maybe not classfiles

--- mason/ec/util/ParamClassLoadException.java
+++ mason/ec/util/ParamClassLoadException.java
@@ -1,7 +1,9 @@
+package ec.util;
 public class ParamClassLoadException extends Exception{
     public ParamClassLoadException(){
+        super();
     }
     public ParamClassLoadException(String msg){
-        return msg;
+        super(msg);
     }
 }
