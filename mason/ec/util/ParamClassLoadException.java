e9211d57ef913612b21bfb5113f850070edccf8b
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2019-02-11 12:47:30 -0500

    ParamClassLoadException

--- mason/ec/util/ParamClassLoadException.java
+++
@@ -1,9 +0,0 @@
-package ec.util;
-public class ParamClassLoadException extends Exception{
-    public ParamClassLoadException(){
-        super();
-    }
-    public ParamClassLoadException(String msg){
-        super(msg);
-    }
-}
