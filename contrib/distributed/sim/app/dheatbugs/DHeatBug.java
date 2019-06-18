7a62262c6f4a47d86c63b9faaf448b87f70fb95b
RAJDEEP SINGH LATHER <rajdeepslather@gmail.com>
2019-06-18 11:14:52 -0400

    Clean up DHeatBug

--- contrib/distributed/sim/app/dheatbugs/DHeatBug.java
+++ contrib/distributed/sim/app/dheatbugs/DHeatBug.java
@@ -10,48 +10,16 @@ import sim.engine.SimState;
 import sim.engine.Steppable;
 import sim.field.grid.NDoubleGrid2D;
 import sim.util.DoublePoint;
-import sim.util.Interval;
 
 public class DHeatBug implements Steppable {
 	private static final long serialVersionUID = 1;
 
-	public double idealTemp;
-
-	public double getIdealTemperature() {
-		return idealTemp;
-	}
-
-	public void setIdealTemperature(double t) {
-		idealTemp = t;
-	}
-
-	public double heatOutput;
-
-	public double getHeatOutput() {
-		return heatOutput;
-	}
-
-	public void setHeatOutput(double t) {
-		heatOutput = t;
-	}
-
-	public double randomMovementProbability;
-
-	public double getRandomMovementProbability() {
-		return randomMovementProbability;
-	}
-
-	public void setRandomMovementProbability(double t) {
-		if (t >= 0 && t <= 1)
-			randomMovementProbability = t;
-	}
-
-	public Object domRandomMovementProbability() {
-		return new Interval(0.0, 1.0);
-	}
-
 	public int loc_x, loc_y;
 	public boolean isFirstStep = true;
+
+	public double idealTemp;
+	public double heatOutput;
+	public double randomMovementProbability;
 
 	public DHeatBug(double idealTemp, double heatOutput, double randomMovementProbability, int loc_x, int loc_y) {
 		this.heatOutput = heatOutput;
@@ -69,11 +37,8 @@ public class DHeatBug implements Steppable {
 	}
 
 	public void step(final SimState state) {
-		
-		DHeatBugs hb = (DHeatBugs) state;
 
-		int myx = loc_x;
-		int myy = loc_y;
+		DHeatBugs hb = (DHeatBugs) state;
 
 		// Skip addHeat for the first step
 		if (!this.isFirstStep) {
@@ -89,7 +54,7 @@ public class DHeatBug implements Steppable {
 		if (state.random.nextBoolean(randomMovementProbability)) { // go to random place
 			bestx = state.random.nextInt(3) - 1 + loc_x;
 			besty = state.random.nextInt(3) - 1 + loc_y;
-		} else if (hb.valgrid.get(myx, myy) > idealTemp) { // go to coldest place
+		} else if (hb.valgrid.get(loc_x, loc_y) > idealTemp) { // go to coldest place
 			for (int x = -1; x < 2; x++)
 				for (int y = -1; y < 2; y++)
 					if (!(x == 0 && y == 0)) {
@@ -104,7 +69,7 @@ public class DHeatBug implements Steppable {
 							besty = yy;
 						}
 					}
-		} else if (hb.valgrid.get(myx, myy) < idealTemp) { // go to warmest place
+		} else if (hb.valgrid.get(loc_x, loc_y) < idealTemp) { // go to warmest place
 			for (int x = -1; x < 2; x++)
 				for (int y = -1; y < 2; y++)
 					if (!(x == 0 && y == 0)) {
@@ -124,19 +89,26 @@ public class DHeatBug implements Steppable {
 			besty = loc_y;
 		}
 
-		int old_x = loc_x;
-		int old_y = loc_y;
+		final int old_x = loc_x;
+		final int old_y = loc_y;
 		loc_x = hb.valgrid.stx(bestx);
 		loc_y = hb.valgrid.sty(besty);
 
 		try {
-			int dst = hb.p.toPartitionId(new int[] { loc_x, loc_y });
-			if (dst != hb.p.getPid()) {
-				hb.bugs.remove(old_x, old_y, this);
-				hb.queue.migrate(this, dst, new DoublePoint(loc_x, loc_y));
+			int dst = hb.partition.toPartitionId(new int[] { loc_x, loc_y });
+			if (dst != hb.partition.getPid()) {
+//				hb.bugs.remove(old_x, old_y, this);
+
+				if (!hb.bugs.remove(old_x, old_y, this))
+					System.err.println("Failed to remove!");
+				hb.migrator.migrate(this, dst, new DoublePoint(loc_x, loc_y));
 				hb.privBugCount--;
 			} else {
-				hb.bugs.remove(old_x, old_y, this);
+//				hb.bugs.remove(old_x, old_y, this);
+
+				if (!hb.bugs.remove(old_x, old_y, this))
+					System.err.println("Failed to remove!");
+
 				hb.bugs.add(loc_x, loc_y, this);
 				hb.schedule.scheduleOnce(this, 1);
 			}
@@ -144,6 +116,35 @@ public class DHeatBug implements Steppable {
 			e.printStackTrace(System.out);
 			System.exit(-1);
 		}
+	}
+
+	public double getIdealTemperature() {
+		return idealTemp;
+	}
+
+	public void setIdealTemperature(double t) {
+		idealTemp = t;
+	}
+
+	public double getHeatOutput() {
+		return heatOutput;
+	}
+
+	public void setHeatOutput(double t) {
+		heatOutput = t;
+	}
+
+	// public Object domRandomMovementProbability() {
+	// return new Interval(0.0, 1.0);
+	// }
+
+	public double getRandomMovementProbability() {
+		return randomMovementProbability;
+	}
+
+	public void setRandomMovementProbability(double t) {
+		if (t >= 0 && t <= 1)
+			randomMovementProbability = t;
 	}
 
 	public String toString() {
