fc85bbd0a25cf640c19461abf4ca3688fb819b5b
RAJDEEP SINGH LATHER <rajdeepslather@gmail.com>
2019-06-21 23:03:51 -0400

    Added comments to RemoteField

--- contrib/distributed/sim/field/RemoteField.java
+++ contrib/distributed/sim/field/RemoteField.java
@@ -13,5 +13,17 @@ import sim.util.NdPoint;
  * @param <P> The Type of NdPoint to use
  */
 public interface RemoteField<P extends NdPoint> extends Remote {
+
+	/**
+	 * Used internally, returns item(s) stored on the point p if and only if p is
+	 * local to the remote field
+	 *
+	 * @param p location
+	 *
+	 * @return the item(s) stored at the location p
+	 *
+	 * @throws RemoteException If the point requested is not <b>local</B> to the
+	 *                         <b>remote</B> field
+	 */
 	Serializable getRMI(P p) throws RemoteException;
 }
