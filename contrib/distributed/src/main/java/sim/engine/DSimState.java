c62a4a847befc0ee97b3edb25d025998158df7c9
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2021-01-30 15:32:38 -0500

    yo

--- contrib/distributed/src/main/java/sim/engine/DSimState.java
+++ contrib/distributed/src/main/java/sim/engine/DSimState.java
@@ -980,19 +980,4 @@ public class DSimState extends SimState {
 		
 	}
 
-class Pair<A, B> implements Serializable 
-{
-	private static final long serialVersionUID = 1L;
-
-	public final A a;
-	public final B b;
-
-	public Pair(A a, B b) {
-		this.a = a;
-		this.b = b;
-	}
-}
-		
-		
-	
 }
