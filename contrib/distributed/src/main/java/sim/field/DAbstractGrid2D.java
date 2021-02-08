5b6a6df3671ddd0b5f38d9e632dfb7405ba8f1c4
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2021-02-08 10:39:17 -0500

    yo

--- contrib/distributed/src/main/java/sim/field/DAbstractGrid2D.java
+++ contrib/distributed/src/main/java/sim/field/DAbstractGrid2D.java
@@ -47,47 +47,6 @@ public abstract class DAbstractGrid2D extends AbstractGrid2D {
 				grid.inHaloToroidal(x - distance, y + distance) &&
 				grid.inHaloToroidal(x - distance, y - distance);
 		}			
-	
-/*
-	public abstract RemoteFulfillable get(Int2D p);
-
-	public abstract RemoteFulfillable get(Int2D p, T t);
-
-	public void add(Int2D p, T t) 
-
-	public void remove(Int2D p, T t) 
-
-	public boolean removeAllLocal(Int2D p) 
-
-	public boolean removeMultiplyLocal(Int2D p, T t) 
-
-	public boolean removeLocal(Int2D p, T t) 
-
-	public void addLocal(Int2D p, T t) 
-
-	public void setLocal(Int2D p, ArrayList<T> t) 
-
-	public ArrayList<T> getLocal(Int2D p) 
-
-	public boolean containsLocal(Int2D p, T t) 
-
-	public void removeAll(Int2D p) 
-
-	public void addAgent(Int2D p, T agent, double time, int ordering) 
-
-	public void addAgent(Int2D p, T agent, double time, int ordering, double interval) 
-
-	public void removeAgent(Int2D p, T agent) 
-
-	public void removeAllAgentsAndObjects(Int2D p) 
-
-	public void moveAgent(Int2D from, Int2D to, T agent) 
-
-	public int getLocal(Int2D p) 
-	public void setLocal(Int2D p, int t) 
-
-	public void set(Int2D p, int val) 
-*/
 
 
 
