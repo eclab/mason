bf407b045ce83b875743c755968a71361d349740
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2021-02-22 14:48:41 -0500

    removed doloop gunk

--- mason/src/main/java/sim/engine/SimState.java
+++ mason/src/main/java/sim/engine/SimState.java
@@ -363,59 +363,25 @@ public class SimState implements java.io.Serializable
         map.put(float.class, Float.class);
         map.put(double.class, Double.class);
 
-        doLoop(new MakesSimState() {
-            public SimState newInstance(long seed, String[] args) {
-                try {
-                    String ci_s = argumentForKey("-c", args);
-                    if (ci_s == null)
-                        return (SimState)(c.getConstructor(new Class[] { Long.TYPE }).newInstance(new Object[] { Long.valueOf(seed) } ));
-                    else {
-                        // Get the constructor based on index given by "-c"
-                        int ci = Integer.parseInt(ci_s);
-                        Constructor constructor = c.getConstructors()[ci];
+        doLoop(new MakesSimState() 
+        	{
+			public SimState newInstance(long seed, String[] args) 
+				{
+				try {
+					return (SimState)(c.getConstructor(new Class[] { Long.TYPE }).newInstance(new Object[] { Long.valueOf(seed) } ));
+					}
+				catch (Exception e) 
+					{
+					if (e instanceof InvocationTargetException)
+						e.getCause().printStackTrace();
+					throw new RuntimeException("Exception occurred while trying to construct the simulation " + c, e);
+					}
+				}
+				
+			public Class simulationClass() { return c; }
 
-                        // Get the parameter type array of the given constructor
-                        Class[] args_primitive_types = constructor.getParameterTypes();
-                        Object[] args_obj = new Object[args_primitive_types.length];
-
-                        // Get the parameter values given by "-a" and compare its length against the parameter type array
-                        // Here we assume the first argument of the constructor will always be the seed
-                        // and the seed value will not be included in the "-a"
-                        String args_value_s = argumentForKey("-a", args);
-                        if (args_value_s == null)
-                            throw new RuntimeException("need to provide arguments to the constructors with -a ");
-                        String[] args_value = args_value_s.split(",");
-                        if (args_value.length != args_primitive_types.length - 1)
-                            throw new RuntimeException("incorrect number of arguments is provided to the given constructor Want: " + (args_primitive_types.length - 1) + " Got: " + args_value.length);
-
-                        // Create the parameter object array based on the type and value
-                        args_obj[0] = seed;
-                        for(int i = 0; i < args_value.length; i++) {
-                            // Support primitive type only
-                            if (!args_primitive_types[i + 1].isPrimitive())
-                                throw new RuntimeException("Unsupported type: " + args_primitive_types[i + 1] + " Primitive type arguments only.");
-
-                            // Boxing of the primitive types
-                            Class args_wrapper_type = map.get(args_primitive_types[i + 1]);
-                            Method valueOf_method = args_wrapper_type.getMethod("valueOf", String.class);
-                            args_obj[i + 1] = valueOf_method.invoke(args_wrapper_type, args_value[i]);
-                            }
-
-                        // Invoke the constructor and return the instance
-                        return (SimState)constructor.newInstance(args_obj);
-                        }
-
-                    }
-                catch (Exception e) {
-                    if(e instanceof InvocationTargetException)
-                        e.getCause().printStackTrace();
-                    throw new RuntimeException("Exception occurred while trying to construct the simulation " + c, e);
-                    }
-                }
-            public Class simulationClass() { return c; }
-
-            public Constructor[] getConstructors() { return c.getConstructors(); }
-            }, args);
+			public Constructor[] getConstructors() { return c.getConstructors(); }
+			}, args);
         }
     
     /** A convenient top-level loop for the simulation command-line.  Takes a MakesSimState which is
