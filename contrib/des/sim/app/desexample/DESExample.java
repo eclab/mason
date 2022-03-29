befa20586ece4dc271126ece69d9888dbc6b8594
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2022-03-29 13:47:13 -0400

    yo

---
+++ contrib/des/sim/app/desexample/DESExample.java
@@ -0,0 +1,79 @@
+/*
+  Copyright 2006 by Sean Luke and George Mason University
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+
+package sim.app.desexample;
+import sim.engine.*;
+import sim.util.*;
+import sim.field.continuous.*;
+import sim.field.network.*;
+import sim.des.*;
+import sim.des.network.*;
+
+public class DESExample extends SimState
+    {
+    private static final long serialVersionUID = 1;
+
+    public DES2D field = new DES2D(100, 100);
+    
+    public DESExample(long seed)
+        {
+        super(seed);
+        }
+                
+    public void start()
+        {
+        super.start();
+        
+        // Make a typical resource
+    	Money quatloos = new Money("Q", 0);
+        
+        // Add some sources
+        Source source1 = new Source(this, quatloos);
+        source1.setRate(1.0, false);
+        source1.setProduction(1.1);
+        source1.setAutoSchedules(true);
+        // source1 needs to be added to the schedule ONCE -- thereafter it'll operate via autoscheduling
+        schedule.scheduleOnce(Schedule.EPOCH, source1);
+        	
+        Source source2 = new Source(this, quatloos);
+        source2.setRate(1.1, false);
+        source2.setProduction(0.7);
+        source2.setAutoSchedules(true);
+        // source1 needs to be added to the schedule ONCE -- thereafter it'll operate via autoscheduling
+        schedule.scheduleOnce(Schedule.EPOCH, source2);
+        	
+        // Add a Simple Delay
+        SimpleDelay simpleDelay = new SimpleDelay(this, 2.3, quatloos);
+        // FIME: at present simple delays autoschedule by default
+        // but sources do not, and simple delays don't need to be added
+        // to the schedule.  This is a bit confusing.
+
+        // Add a Sink
+        Sink sink = new Sink(this, quatloos);
+        
+        // Hook them up
+        source1.addReceiver(simpleDelay);
+        source2.addReceiver(simpleDelay);
+        simpleDelay.addReceiver(sink);
+        
+        // Set up our network for display purposes
+        field = new DES2D(100, 100);
+        field.add(source1, 20, 40);
+        field.add(source2, 20, 60);
+        field.add(simpleDelay, 50, 50);
+        field.add(sink, 80, 50);
+        /// FIXME: maybe we should automatically connect somehow?
+        field.connect(source1, simpleDelay);
+        field.connect(source2, simpleDelay);
+        field.connect(simpleDelay, sink);
+        }
+        
+    public static void main(String[] args)
+        {
+        doLoop(DESExample.class, args);
+        System.exit(0);
+        }    
+    }
---
+++ contrib/des/sim/app/desexample/DESExampleWithUI.java
@@ -0,0 +1,107 @@
+/*
+  Copyright 2006 by Sean Luke and George Mason University
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+
+package sim.app.desexample;
+import sim.portrayal.network.*;
+import sim.portrayal.continuous.*;
+import sim.engine.*;
+import sim.display.*;
+import sim.portrayal.simple.*;
+import sim.portrayal.*;
+import javax.swing.*;
+import java.awt.Color;
+import java.awt.*;
+import sim.field.network.*;
+
+public class DESExampleWithUI extends GUIState
+    {
+    public Display2D display;
+	public JFrame displayFrame;
+	
+    ContinuousPortrayal2D layoutPortrayal = new ContinuousPortrayal2D();
+    NetworkPortrayal2D graphPortrayal = new NetworkPortrayal2D();
+        
+    public static void main(String[] args)
+        {
+        DESExampleWithUI app = new DESExampleWithUI();
+        Console c = new Console(app);
+        c.setVisible(true);
+        }
+
+    public DESExampleWithUI() { super(new DESExample( System.currentTimeMillis())); }
+    public DESExampleWithUI(SimState state) { super(state); }
+
+    public Object getSimulationInspectedObject() { return state; }
+
+	// make the main model inspector volatile so it updates each time?
+    /*
+    public Inspector getInspector()
+        {
+        Inspector i = super.getInspector();
+        i.setVolatile(true);
+        return i;
+        }
+    */
+
+    public static String getName() { return "DES Example"; }
+    
+    public void start()
+        {
+        super.start();
+        setupPortrayals();
+        }
+
+    public void load(SimState state)
+        {
+        super.load(state);
+        setupPortrayals();
+        }
+
+    public void setupPortrayals()
+        {
+        DESExample example = (DESExample) state;
+                
+        layoutPortrayal.setField(example.field.getNodes());
+        layoutPortrayal.setPortrayalForAll(new RectanglePortrayal2D(5.0, false));
+        
+        SimpleEdgePortrayal2D edge = new SimpleEdgePortrayal2D(Color.BLUE, Color.RED, Color.BLACK, new Font("SansSerif", Font.PLAIN, 2));
+        edge.setShape(SimpleEdgePortrayal2D.SHAPE_LINE);
+        graphPortrayal.setField(example.field.getField());
+        graphPortrayal.setPortrayalForAll(edge);
+
+        // reschedule and repaint the displayer
+        display.reset();
+        display.setBackdrop(Color.white);
+        display.repaint();
+        }
+
+    public void init(Controller c)
+        {
+        super.init(c);
+
+        // make the displayer
+        display = new Display2D(600,600,this);
+        // turn off clipping
+        display.setClipping(false);
+        display.attach( layoutPortrayal, "Layout" );
+        display.attach( graphPortrayal, "Connections" );
+
+        displayFrame = display.createFrame();
+        displayFrame.setTitle("Amazing!");
+        c.registerFrame(displayFrame);   // register the frame so it appears in the "Display" list
+        displayFrame.setVisible(true);
+        }
+
+    public void quit()
+        {
+        super.quit();
+
+        if (displayFrame!=null) displayFrame.dispose();
+        displayFrame = null;
+        display = null;
+        }
+
+    }
