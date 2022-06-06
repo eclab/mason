18a6ff3187a9e668c5dd059a6b5e00a2254c4837
SeanLuke <34138957+SeanLuke@users.noreply.github.com>
2022-06-06 18:30:39 -0400

    yo

---
+++ contrib/des/sim/des/Parented.java
@@ -0,0 +1,28 @@
+/*
+  Copyright 2021 by Sean Luke and George Mason University
+  Licensed under the Academic Free License version 3.0
+  See the file "LICENSE" for more information
+*/
+
+package sim.des;
+
+import sim.engine.*;
+import sim.util.*;
+import java.util.*;
+
+/**
+   A simple interface for objects which have parents and names
+*/
+
+public interface Parented extends Named
+    {
+    /**
+       Returns the object's parent.
+    */
+    public Object getParent();
+
+    /**
+       Sets the object's parent.
+    */
+    public void setParent(Object parent);
+    }
